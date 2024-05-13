caae61ae7fe6e6a7c49d1b811b4eb53bcaff587b
vivekolladapu328@gmail.com <vivekolladapu>
2024-05-13 15:56:39 +1000

    updated login and sign page

--- public/JS/password-view.js
+++ public/JS/password-view.js
@@ -3,12 +3,43 @@ const BtnSet1 = document.querySelector('.btn-set1');
 const BtnSet2 = document.querySelector('.btn-set2');
 
 
-// add entry button - opens add window
 document.querySelector('.add-record').addEventListener('click', function(){
   AddWall.style.display = 'flex';
 });
 
-// add window close button
 document.querySelector('.add-cancel').addEventListener('click', function(){
   AddWall.style.display = 'none';
 });
+
+function addRecord(event) {
+  event.preventDefault(); 
+
+  var platform = document.querySelector(".add-platform").value;
+  var username = document.querySelector(".add-username").value;
+  var password = document.querySelector(".add-password").value;
+  var retypePassword = document.querySelector(".add-retype-password").value;
+
+  if (password !== retypePassword) {
+      alert("Passwords do not match!");
+      return;
+  }
+
+  var tableBody = document.querySelector(".styled-table tbody");
+  var newRow = tableBody.insertRow();
+  newRow.innerHTML = `
+      <tr>
+      <th class="selection">Selection</th>
+      <td>${platform}</td>
+      <td>${username}</td>
+      <td>${password}</td>
+      </tr>
+  `;
+  
+
+  document.querySelector(".add-platform").value = "";
+  document.querySelector(".add-username").value = "";
+  document.querySelector(".add-password").value = "";
+  document.querySelector(".add-retype-password").value = "";
+
+  hideAddRecordWindow();
+}
